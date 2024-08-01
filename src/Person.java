f5618b71cfe9e9480015de9b095bbace0403129f
Niranjan0505 <ezhil.niranjan@gmail.com>
2024-07-31 22:26:08 -0400

    Person class add

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+   private String name;
+   private int age;
+   private String gender;
+
+    public void Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void displayPerson() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+    }
+public String getName() {
+        return name;
+}
+public void setName(String name) {
+        this.name = name;
+}
+public int getAge() {
+        return age;
+}
+public void setAge(int age) {
+        this.age = age;
+}
+}
