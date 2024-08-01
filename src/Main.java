7b1cc2c8fd48139b8d0698bc481e36292130739f
keerthiranji <kiruthiga.ranjithb@gmail.com>
2024-08-01 18:22:48 -0400

    Adds Student class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        Student a1 = new Student("KAVI", 6, 'F', "1st");
+
+        System.out.println("Name: " + (a1.getName()));
+        System.out.println("Age: " + (a1.getAge()));
+        System.out.println("Gender: " + (a1.getGender()));
+        System.out.println("Grade: " + (a1.getGrade()));
+
+
+        //a1.setName("sham");
+
+
+
+
+
+
 
     }
 }
